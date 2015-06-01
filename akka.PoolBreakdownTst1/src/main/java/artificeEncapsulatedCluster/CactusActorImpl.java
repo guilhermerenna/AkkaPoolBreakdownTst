e8726f017851f4c88a2cb387bcec0eb2b76d8f3e
Guilherme Renna Rodrigues <guilhermerenna@gmail.com>
2015-06-01 07:57:48 -0300

    Interfaced Cactus atualizado.

--- akka.PoolBreakdownTst1/src/main/java/artificeEncapsulatedCluster/CactusActorImpl.java
+++ akka.PoolBreakdownTst1/src/main/java/artificeEncapsulatedCluster/CactusActorImpl.java
@@ -43,26 +43,4 @@ public class CactusActorImpl extends WorldObjectActor  {
             ref.tell(stimulus, getSelf());
         }
     }
-
-    //	public CactusActorImpl(AtomicReference proxyVar, Function0 createInstance,
-//			Seq interfaces) {
-//		super(proxyVar, createInstance, interfaces);
-//		// TODO Auto-generated constructor stub
-//	}
-
-//	@Override
-//	public void onReceive(Object arg0) throws Exception {
-//		if(arg0 instanceof LuminousStimulusMessage) {
-//			System.out.println(this.getSelf().toString()+"A can see something! "+((LuminousStimulusMessage) arg0).getMessage()+"! Ref.: " + ((StimulusMessage) arg0).getSequenceNumber());
-//		} else if(arg0 instanceof TouchStimulusMessage) {
-//			System.out.println(this.getSelf().toString()+": "+getSender().toString()+" toched me! Spiking it back! =P Ref.: " + ((StimulusMessage) arg0).getSequenceNumber());
-//			getSender().tell(new SpikeStimulusMessage("Don't touch me! I am a cactus!"),this.getSelf());
-//			getSender().forward(new SpikeStimulusMessage("Don't touch me! I am a cactus!"),this.context());
-//		} else if(arg0 instanceof StimulusMessage) {
-//			System.out.println("Unknown stimulus received.\n"+((StimulusMessage)arg0).getMessage()+"\nDiscarding ref. " + ((StimulusMessage) arg0).getMessage());
-//		} else {
-//			throw new Exception("Message type not supported.");
-//		}
-//
-//	}
 }
