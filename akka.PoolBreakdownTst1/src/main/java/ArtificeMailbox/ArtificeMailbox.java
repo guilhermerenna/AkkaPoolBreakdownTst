38469e6766901c99b8ced21f50b612c75081f99f
Guilherme Renna Rodrigues <guilhermerenna@gmail.com>
2015-06-03 07:52:09 -0300

    Added Mailbox

--- akka.PoolBreakdownTst1/src/main/java/ArtificeMailbox/ArtificeMailbox.java
+++ akka.PoolBreakdownTst1/src/main/java/ArtificeMailbox/ArtificeMailbox.java
@@ -1,24 +1,29 @@
-package ArtificeMailbox;
+package mailbox;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.dispatch.Envelope;
 import akka.dispatch.MailboxType;
 import akka.dispatch.MessageQueue;
 import akka.dispatch.ProducesMessageQueue;
 import com.typesafe.config.Config;
-import ArtificeMailbox.messageQueue.GroupedMessageQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Queue;
+
+import mailbox.messageQueue.GroupedMessageQueue;
 import scala.Option;
 
-public class MyUnboundedMailbox implements MailboxType,
+public class ArtificeMailbox implements MailboxType,
         ProducesMessageQueue<GroupedMessageQueue> {
 
     // This constructor signature must exist, it will be called by Akka
-    public MyUnboundedMailbox(ActorSystem.Settings settings, Config config) {
-        // put your initialization code here
+    public ArtificeMailbox(ActorSystem.Settings settings, Config config) {
+        System.out.println("Initializing Mailbox...");
     }
 
     // The create method is called to create the MessageQueue
     public MessageQueue create(Option<ActorRef> owner, Option<ActorSystem> system) {
+        System.out.println("Creating GroupedMessageQueue...");
         return new GroupedMessageQueue();
     }
 }
---
+++ akka.PoolBreakdownTst1/src/main/java/ArtificeMailbox/messageQueue/ArtificeMessageQueueSemantics.java
@@ -0,0 +1,5 @@
+package mailbox.messageQueue;
+
+// Marker interface used for mailbox requirements mapping
+public interface ArtificeMessageQueueSemantics {
+}
